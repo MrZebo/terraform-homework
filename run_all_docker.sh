0d757a06ba7df29d35f39439face3fd03dceb7bf
Hrebennikov Borys <bhrebe@softserveinc.com>
2022-02-28 18:09:23 +0200

    second commit

--- run_all_docker.sh
+++ run_all_docker.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-if [ -z "$1" ]
-  then
-    RESOURCES_FILE="resources.txt"
-else
-    RESOURCES_FILE=$1
-fi
+AR=("file1.txt" "file2.txt" "file3.txt" "file4.txt")
 
-# AR=("file1.txt" "file2.txt" "file3.txt" "file4.txt")
-AR=(0 50 100 150 200 250 300 350 400)
+NUMBER=$(shuf -i 0-3 -n 1)
 
-
-NUMBER=$(shuf -i 0-8 -n 1)
-
-ARR_INDEX=${AR[NUMBER]}
-# RESOURCES_FILE=${AR[NUMBER]}
+RESOURCES_FILE=${AR[NUMBER]}
 
 echo "File: $RESOURCES_FILE"
 
 IFS=$'\n' read -d '' -r -a lines < $RESOURCES_FILE
 
-# for i in "${lines[@]}"
-for ((i=$ARR_INDEX;i<=$ARR_INDEX+50;i++));
+for i in "${lines[@]}"
 do
    echo "$i"
    export URL=$i
